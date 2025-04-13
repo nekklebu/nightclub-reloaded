56771526d291fd96115ed878933a4b0dd56456e8
Nikki <nikkibuqu@gmail.com>
2025-04-13 18:35:02 -0400

    raw views set up, to be stylized

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import BlogPost
 
-# Create your views here.
+class BlogListView(ListView):
+    model = BlogPost
+    template_name = 'blog/blog_list.html'
+    context_object_name = 'posts'
+
+class BlogDetailView(DetailView):
+    model = BlogPost
+    template_name = 'blog/blog_detail.html'
--- portfolio/views.py
+++ portfolio/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Project
 
-# Create your views here.
+class ProjectListView(ListView):
+    model = Project
+    template_name = 'portfolio/project_list.html'
+    context_object_name = 'projects'
+
+class ProjectDetailView(DetailView):
+    model = Project
+    template_name = 'portfolio/project_detail.html'
