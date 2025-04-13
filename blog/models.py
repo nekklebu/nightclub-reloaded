e72b64d7c38475cb68b7283c633e679b70bfc5f5
Nikki <nikkibuqu@gmail.com>
2025-04-13 17:57:30 -0400

    We're rebooting the ollll' nightclub. Just a personal website to deepen my knowledge and play with django features & fun ideas

---
+++ blog/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+class BlogPost(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    body = models.TextField()
+    date_posted = models.DateTimeField(auto_now_add=True)
+    
+    def __str__(self):
+        return self.title
