56673bd091a92c414835da53da43d47e2a1d3af7
nekklebu <92735679+nekklebu@users.noreply.github.com>
2025-04-15 03:18:56 -0400

    Adding basic blog image support (#3)
    * Removing; Images will be added but not as a part of testing
    
    * Adding some initial view testing
    
    * Extending blog model w/ images
    
    * Added basic image support for blog posts
    
    * Updated view testing to check for images

--- conftest.py
+++ conftest.py
@@ -1,8 +1,16 @@
 import pytest
-
+import io
+from PIL import Image
+from django.core.files.uploadedfile import SimpleUploadedFile
+from django.utils.text import slugify
 from blog.models import BlogPost
 from portfolio.models import Project
-from django.utils.text import slugify
+
+def _that_one_picture(name="from-that-one-time.jpg"):
+    img = Image.new('RGB', (100, 100), color=(255, 0, 0))
+    buf = io.BytesIO()
+    img.save(buf, format='JPEG')
+    return SimpleUploadedFile(name, buf.getvalue(), content_type='image/jpeg')
 
 @pytest.fixture
 def that_one_time():
@@ -11,7 +19,8 @@ def that_one_time():
     return BlogPost.objects.create(
             title=title,
             slug=slugify(title),
-            body=body
+            body=body,
+            image=_that_one_picture()
             )
 
 @pytest.fixture
--- tests/test_views.py
+++ tests/test_views.py
@@ -19,7 +19,6 @@ def test_project_displayed(plastic_cups, client):
 def test_project_details_displayed(plastic_cups, client):
     response = client.get(reverse('project-detail', kwargs={'pk': plastic_cups.pk}))
     assert plastic_cups.title in response.content.decode()
-    print(response.content.decode())
     assert plastic_cups.description in response.content.decode()
 
 @pytest.mark.django_db
@@ -33,3 +32,4 @@ def test_post_displayed(that_one_time, client):
     response = client.get(reverse('blog-detail', kwargs={'slug':that_one_time.slug}))
     assert that_one_time.title in response.content.decode()
     assert that_one_time.body in response.content.decode()
+    assert that_one_time.image.url in response.content.decode()
