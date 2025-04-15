8f310125ceaecb5db2d2ad93fd9ede00227caa59
nekklebu <92735679+nekklebu@users.noreply.github.com>
2025-04-15 08:59:39 -0400

    Prepping for the cloud (#6)
    <<dotenv
    >>django-environ

--- thenightclub/settings.py
+++ thenightclub/settings.py
@@ -12,25 +12,26 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
-import dj_database_url
-from dotenv import load_dotenv, dotenv_values
+import environ
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-load_dotenv(BASE_DIR / '.env')
+
+env = environ.Env()
+environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv("SECRET_KEY")
+SECRET_KEY = env("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DEBUG", "False")
+DEBUG = env.bool("DEBUG", default="False")
 
-ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "*").split(",")
+ALLOWED_HOSTS = env("ALLOWED_HOSTS", default="*").split(",")
 
 
 # Application definition
