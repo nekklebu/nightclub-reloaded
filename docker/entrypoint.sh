f576015976eae1ab107ca1c5085f2d489a1a6521
Nikki <nikkibuqu@gmail.com>
2025-04-15 21:20:31 -0400

    some small script changes that prep us for living in google's cute lil cloud

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 python manage.py migrate
-python manage.py runserver 0.0.0.0:${PORT:-8000}
+gunicorn thenightclub.wsgi:application \
+    --bind 0.0.0.0:${PORT:-8080} \
+    --workers 3
