602d572026a8e1af4258b71e5ee637c18b350cd6
Nikki <nikkibuqu@gmail.com>
2025-04-15 21:16:43 -0400

    A full rework of the development script. This is currently a "build and run" script with plenty of room for improvement. But I know confidently that this script serves two operational images+containers for the website and static content server. So let's get that locked in and move from there

--- docker/dev-local.ps1
+++ docker/dev-local.ps1
@@ -1,32 +1,90 @@
 # dev-local.ps1
 param (
-        [switch]$detach,
+        [switch]$detach=$True,
+        [switch]$local,
         [switch]$kill,
-        [int]$port=8000,
-        [string]$name='club-container'
+        [switch]$dryRun,
+        [ValidateSet("all", "nightclub-site", "content-server")]
+        [string]$target="all"
       )
 
 $ErrorActionPreference = "Stop"
+$PORT_SITE = "8080"
+$PORT_SERV = "9090"
+$detachPreference = if ( $detach ) { "-d" } else { "" }
+$projContainerMap = @{"nightclub-site"="club-container"; "content-server"="content-container"}
 
-$detachPreference=""
-if ( $detach ) {
-    $detachPreference="-d"
+Function Get-DockerNormalizedPath {
+    param ([string]$path)
+    return (Resolve-Path $path).Path -replace '\\', '/'
+    }
+
+Function Build {
+    param (
+        [string]$target
+          )
+
+    if ( $target -eq "all" ) {
+        Build -target "nightclub-site"
+        Build -target "content-server"
+        return
+    }
+
+    if ( $target -eq "nightclub-site" ) {
+        $projectroot = "./"
+        $port = $PORT_SITE
+
+#        TEMP disabled @TODO this doesnt collect anything
+#        with my project structured how it is
+#        Write-Host "Collecting static files,,,"
+#        python manage.py collectstatic --clear --no-input
+
+    } elseif ( $target -eq "content-server" ) {
+        $projectroot="./static_server"
+        $port = $PORT_SERV
+    }
+
+    # normalize path for docker
+    $hostroot   = Get-DockerNormalizedPath $projectroot
+    $dockerfile = "$hostroot/Dockerfile"
+    $envfile    = "$hostroot/.env"
+
+    Write-Host "Building container image,,,"
+    Write-Host "docker build -f $dockerfile -t $target ."
+    if ( -Not $dryRun ) {
+        docker build -f $dockerfile -t $target .
+    }
+
+    $volumeFlag = "-v"
+    $volumeValue= "${hostroot}:/app"
+    $volumeArg = if ( $local ) { "$volumeFlag $volumeValue" } else {""}
+    $runArgs = @(
+    "run",
+    $detachPreference,
+    "-p", "${port}:${port}",
+    "--env-file", $envfile,
+    $volumeArg,
+    "--name", $projContainerMap[$target],
+    $target
+            )
+
+    Write-Host "Running container,,,"
+    Write-Host ("docker " + $($runArgs -join ' '))
+    if ( -Not $dryRun ) {
+        docker @runArgs
+    }
+
+    return
 }
 
-Write-Host "Stopping any currently running containers,,,"
-try {
-    docker stop ${name} *>$null
-    docker rm ${name}   *>$null
-} catch {}
+if ( -Not $dryRun ) {
+    Write-Host "Stopping+removing any currently running containers,,,"
+    try {
+        docker ps -q | ForEach-Object { docker stop $_ }
+        docker ps -a -q | ForEach-Object { docker rm $_ }
+    } catch {}
 
-if ( $kill ) { return }
+    if ( $kill ) { return }
+}
 
-Write-Host "Building container image,,,"
-docker build -t nightclub-site .
-
-Write-Host "Running container,,,"
-docker run ${detachPreference} -p ${port}:${port} `
---env-file .env `
--v ${PWD}:/app `
---name ${name} `
-nightclub-site
+Build -target $target
